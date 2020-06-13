5e299239284765f504596f54d439c692e8c7bf4f
Colin Higgins <colin-higgins@users.noreply.github.com>
2020-06-13 16:17:00 -0400

    Create colorcode.js

---
+++ colorcode.js
@@ -0,0 +1,84 @@
+
+var colorCode = colorCode || {};
+
+colorCode.htmlBasis = colorCode.htmlBasis || '<table>'
++ '  <thead>'
++ '    <tr>'
++ '      <th>Name</th>'
++ '      <th>Domain</th>'
++ '      <th>Address</th>'
++ '      <th>Contact</th>'
++ '      <th>Site</th>'
++ '      <th>Social</th>'
++ '    </tr>'
++ '  </thead>'
++ '  <tbody>'
++ '  {list}'
++ '  </tbody>'
++ '</table>'
+
+colorCode.make = colorCode.make || function (name) {
+    var instance = {
+      name: name,
+      domain: 'N/A',
+      address: 'N/A',
+      contact: 'N/A',
+      site: 'N/A',
+      social: 'N/A'
+    };
+    
+    var replaceWithProperty = function (text, key) {
+      var value = instance[key];
+      return text.replace('{' + key + '}', value);
+    }
+    
+    instance.render = function () {
+      var rowHtml = '<tr><td>{name}</td><td>{domain}</td><td>{address}</td><td>{contact}</td><td>{site}</td><td>{social}</td></tr>';
+      rowHtml = replaceWithProperty(rowHtml, 'name'); 
+      rowHtml = replaceWithProperty(rowHtml, 'domain'); 
+      rowHtml = replaceWithProperty(rowHtml, 'address'); 
+      rowHtml = replaceWithProperty(rowHtml, 'contact'); 
+      rowHtml = replaceWithProperty(rowHtml, 'site'); 
+      rowHtml = replaceWithProperty(rowHtml, 'social'); 
+      return rowHtml;
+    };
+    
+    instance.domain = function (domain) {
+      instance.domain = domain;
+      return instance;
+    };
+    
+    instance.address = function (address) {
+      instance.address = address;
+      return instance;
+    };
+    
+    instance.contact = function (contact) {
+      instance.contact = contact;
+      return instance;
+    };
+    
+    instance.site = function (site) {
+      instance.site = site;
+      return instance;
+
+    };
+    
+    instance.social = function (social) {
+      instance.social = social;
+      return instance;
+    };
+    
+    return instance;
+};
+
+colorCode.render = colorCode.render || function () {
+    var rowsHtml = '';
+    for (var i = 0; i < colorCoded.length; i++) {
+      rowsHtml = rowsHtml.concat(colorCoded[i].render());
+    }
+    
+	html = colorCode.htmlBasis.replace('{list}', rowsHtml);
+    var container = document.getElementById('ColorCode');
+    container.innerHTML = html;
+ };
