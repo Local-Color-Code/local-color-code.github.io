c9db2693d9f2f29a32812819c4ed326d5207800b
Colin Higgins <colin-higgins@users.noreply.github.com>
2020-06-13 16:28:09 -0400

    Update areacode.js

--- 716/areacode.js
+++ 716/areacode.js
@@ -1,12 +1,3 @@
-areaCode = { 
-  businesses: [],
-  make: function (name) {
-  	var instance = colorCode.make(name);
-	businesses.add(instance);
-	return instance;
-  }
-};
-
-areaCode.make('Nakiea Cook, MBA').domain('Accountant').address('N/A').contact('info@nakieacook.com').site('https://www.nakieacook.com/').social('https://www.facebook.com/nakiea.adger');
+colorCode.make('Nakiea Cook, MBA').domain('Accountant').address('N/A').contact('info@nakieacook.com').site('https://www.nakieacook.com/').social('https://www.facebook.com/nakiea.adger');
 
 colorCode.render();
