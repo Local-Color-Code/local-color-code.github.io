8f8a8efc0f1b65fbc069b4df9b2b45b0674c61a9
Colin Higgins <colin-higgins@users.noreply.github.com>
2020-06-13 22:39:36 -0400

    Update colorcode.js

--- colorcode.js
+++ colorcode.js
@@ -15,12 +15,12 @@ colorCode.htmlBasis = colorCode.htmlBasis || '<table>'
 
 colorCode.make = colorCode.make || function (name) {
     var instance = {
-      name: name,
-      domain: '',
-      address: '',
-      contact: '',
-      site: '',
-      social: ''
+      nameText: name,
+      domainText: '',
+      addressText: '',
+      contactText: '',
+      siteText: '',
+      socialText: ''
     };
 	
 	var contains = function(stuff, search) {
@@ -41,71 +41,71 @@ colorCode.make = colorCode.make || function (name) {
 	};
     
     instance.render = function () {
-      var rowHtml = '<tr><td>{name}</td><td>{secondColumn}</td><td>{thirdColumn}</td></tr>';
+      var rowHtml = '<tr><td>{nameText}</td><td>{secondColumn}</td><td>{thirdColumn}</td></tr>';
 	  var secondColumn = '';
 	  
 	  if (instance.domain.length > 0) {
-		  secondColumn = secondColumn.concat(instance.domain, '<br />');
+		  secondColumn = secondColumn.concat(instance.domainText, '<br />');
 	  }
 	  if (instance.address.length > 0) {
-		  secondColumn = secondColumn.concat(instance.address, '<br />');
+		  secondColumn = secondColumn.concat(instance.addressText, '<br />');
 	  }
 	  if (instance.contact.length > 0) {
-		  secondColumn = secondColumn.concat(instance.contact);
+		  secondColumn = secondColumn.concat(instance.contactText);
 	  }
 	  
 	  var thirdColumn = '';
 
 	  if (instance.site.length > 0) {
-		  thirdColumn = thirdColumn.concat(instance.site, '<br />');
+		  thirdColumn = thirdColumn.concat(instance.siteText, '<br />');
 	  }
 	  if (instance.social.length > 0) {
-		  thirdColumn = thirdColumn.concat(instance.social);
+		  thirdColumn = thirdColumn.concat(instance.socialText);
 	  }
 	  
-      rowHtml = replaceWithProperty(rowHtml, 'name'); 
+      rowHtml = replaceWithProperty(rowHtml, 'nameText'); 
       rowHtml = rowHtml.replace('{secondColumn}', secondColumn); 
       rowHtml = rowHtml.replace('{thirdColumn}', thirdColumn); 
 	  
       return rowHtml;
     };
     
-    instance.domain = function (domain) {
-	  domain = checkEmpty(domain);
-      instance.domain = domain;
+    instance.domain = function (domainText) {
+	  domainText = checkEmpty(domainText);
+      instance.domainText = domainText;
       return instance;
     };
     
-    instance.address = function (address) {
-	  address = checkEmpty(address);
-      instance.address = address;
+    instance.address = function (addressText) {
+	  addressText = checkEmpty(addressText);
+      instance.addressText = addressText;
       return instance;
     };
     
-    instance.contact = function (contact) {
-	  contact = checkEmpty(contact);
-      instance.contact = contact;
+    instance.contact = function (contactText) {
+	  contactText = checkEmpty(contact);
+      instance.contactText = contactText;
       return instance;
     };
     
-    instance.site = function (site) {
-	  site = checkEmpty(site);
-	  if (contains(site, "http")) {
-	    site = '<a href="' + site + '" target="_blank">Website</a>';
+    instance.site = function (siteText) {
+	  siteText = checkEmpty(siteText);
+	  if (contains(siteText, "http")) {
+	    siteText = '<a href="' + siteText + '" target="_blank">Website</a>';
 	  }
-      instance.site = site;
+      instance.siteText = siteText;
       return instance;
     };
     
-    instance.social = function (social) {
-	  social = checkEmpty(social);
-	  if (contains(social,'fb.co') || contains(social, 'facebook.co')) {
-	    social = '<a href="' + social + '" target="_blank">Facebook</a>';
+    instance.social = function (socialText) {
+	  socialText = checkEmpty(socialText);
+	  if (contains(socialText,'fb.co') || contains(socialText, 'facebook.co')) {
+	    socialText = '<a href="' + social + '" target="_blank">Facebook</a>';
 	  }
-	  if (contains(social,'ig.co') || contains(social, 'instagram.co')) {
-	    social = '<a href="' + social + '" target="_blank">Instagram</a>';
+	  if (contains(socialText,'ig.co') || contains(socialText, 'instagram.co')) {
+	    socialText = '<a href="' + socialText + '" target="_blank">Instagram</a>';
 	  }
-      instance.social = social;
+      instance.socialText = socialText;
       return instance;
     };
 	
